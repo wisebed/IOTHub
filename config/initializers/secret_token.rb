d210e81b491e1f53b6d52f30015dc35831b0e6c9
Marv Cool <marv@hostin.is>
2012-05-11 12:28:54 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IOTHub::Application.config.secret_token = 'e3422d6af565aa727529f57e0951605e59b88be940801156f0e3bd0c43de041326d83fcc91ed1466c8d8b3d5de2c33a171600ed975ae8a59177fb931be002bb8'
