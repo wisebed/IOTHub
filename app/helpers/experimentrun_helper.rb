84e3c008b190b11cee505495d92538631e5a4d25
Marv Cool <marv@hostin.is>
2012-12-21 11:01:48 +0100

    moves static file path generation
    adds new heler method that generates the path for an ExperimentRuns
    static files

--- app/helpers/experimentrun_helper.rb
+++ app/helpers/experimentrun_helper.rb
@@ -1,2 +1,7 @@
 module ExperimentrunHelper
+
+  def experimentrun_staic_path(run,filename)
+    run = ExperimentRun.find(run) unless run.is_a? ExperimentRun
+    experiment_run_path(run.experiment,run)+"/static/"+filename
+  end
 end
