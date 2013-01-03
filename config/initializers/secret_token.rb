5ee99397b8b2a2551863abfa457c177951f7e2ee
Marv Cool <marv@hostin.is>
2013-01-03 15:20:29 +0100

    thanks joernchen, keep on hacking! :)

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,14 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-IOTHub::Application.config.secret_token = 'e3422d6af565aa727529f57e0951605e59b88be940801156f0e3bd0c43de041326d83fcc91ed1466c8d8b3d5de2c33a171600ed975ae8a59177fb931be002bb8'
+# IOTHub::Application.config.secret_token = 'e3422d6af565aa727529f57e0951605e59b88be940801156f0e3bd0c43de041326d83fcc91ed1466c8d8b3d5de2c33a171600ed975ae8a59177fb931be002bb8'
+
+token_file = File.join(Rails.root,"config","secret_token.yml")
+begin
+  IOTHub::Application.config.secret_token = YAML.load(File.open(token_file))
+rescue LoadError, Errno::ENOENT => e
+  File.open(token_file, 'w' ) do |out|
+    YAML.dump(`rake secret`.strip, out )
+  end
+  IOTHub::Application.config.secret_token = YAML.load(File.open(token_file))
+end
