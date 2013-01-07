048f086f3cef62aa060e3e12b47f195a9caed134
Marv Cool <marv@hostin.is>
2013-01-07 17:22:25 +0100

    adds a select-tag for testbeds for new runs
    this closes #1

--- app/models/user_testbed_credential.rb
+++ app/models/user_testbed_credential.rb
@@ -3,4 +3,8 @@ class UserTestbedCredential < ActiveRecord::Base
   belongs_to :testbed
 
   attr_accessible :username, :password, :testbed_id
+
+  def shortname
+    self.testbed.shortname
+  end
 end
