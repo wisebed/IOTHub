934e5d040d4a5de7af01f394892de44d32b27f2d
Marv Cool <marv@hostin.is>
2013-01-28 13:28:28 +0100

    fixes initscript for passenger standallone

--- config/initscript.sh
+++ config/initscript.sh
@@ -1,90 +1,53 @@
 #! /bin/sh
 
 ### BEGIN INIT INFO
-# Provides:          nginx
+# Provides:          iothub
 # Required-Start:    $local_fs $remote_fs $network $syslog
 # Required-Stop:     $local_fs $remote_fs $network $syslog
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: starts the nginx web server
-# Description:       starts nginx using start-stop-daemon
+# Short-Description: starts the iothub application server
+# Description:       starts the iothub application server
 ### END INIT INFO
 
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-
 #### CHANGE THE LINE BELOW!
-DAEMON=/usr/local/nginx/sbin/nginx
+APPDIR=/home/iothub/IOTHub/current/
+PASSENGER=/home/iothub/.rvm/gems/ruby-1.9.3-p362/bin/passenger
+PORT=8887
+USER=iothub
 #### CHANGE THE LINE ABOVE!
 
-NAME=nginx
-DESC=nginx
+NAME=iothub
+DESC=iothub
 
-test -x $DAEMON || exit 0
-
-# Include nginx defaults if available
-if [ -f /etc/default/nginx ] ; then
-	. /etc/default/nginx
-fi
+test -x $PASSENGER || exit 0
 
 set -e
-
-. /lib/lsb/init-functions
-
-test_nginx_config() {
-  if $DAEMON -t $DAEMON_OPTS >/dev/null 2>&1
-  then
-    return 0
-  else
-    $DAEMON -t $DAEMON_OPTS
-    return $?
-  fi
-}
 
 case "$1" in
   start)
 	echo -n "Starting $DESC: "
-        test_nginx_config
-	start-stop-daemon --start --quiet --pidfile /var/run/$NAME.pid \
-		--exec $DAEMON -- $DAEMON_OPTS || true
-	echo "$NAME."
+        cd $APPDIR && $PASSENGER start -p $PORT --user $USER -e production -d
+	echo "[OK]."
 	;;
   stop)
 	echo -n "Stopping $DESC: "
-	start-stop-daemon --stop --quiet --pidfile /var/run/$NAME.pid \
-		--exec $DAEMON || true
-	echo "$NAME."
+	    cd $APPDIR && $PASSENGER stop -p $PORT
+	echo "[OK]."
 	;;
   restart|force-reload)
 	echo -n "Restarting $DESC: "
-	start-stop-daemon --stop --quiet --pidfile \
-		/var/run/$NAME.pid --exec $DAEMON || true
-	sleep 1
-        test_nginx_config
-	start-stop-daemon --start --quiet --pidfile \
-		/var/run/$NAME.pid --exec $DAEMON -- $DAEMON_OPTS || true
-	echo "$NAME."
+        cd $APPDIR && $PASSENGER stop -p $PORT
+    echo -n "[stopped] "
+     sleep 1
+        cd $APPDIR && $PASSENGER start -p $PORT --user $USER -e production -d
+	echo "[started]."
 	;;
-  reload)
-        echo -n "Reloading $DESC configuration: "
-        test_nginx_config
-        start-stop-daemon --stop --signal HUP --quiet --pidfile /var/run/$NAME.pid \
-            --exec $DAEMON || true
-        echo "$NAME."
-        ;;
-  configtest)
-        echo -n "Testing $DESC configuration: "
-        if test_nginx_config
-        then
-          echo "$NAME."
-        else
-          exit $?
-        fi
-        ;;
   status)
-	status_of_proc -p /var/run/$NAME.pid "$DAEMON" nginx && exit 0 || exit $?
+	cd $APPDIR && $PASSENGER status -p $PORT
 	;;
   *)
-	echo "Usage: $NAME {start|stop|restart|reload|force-reload|status|configtest}" >&2
+	echo "Usage: $NAME {start|stop|restart|force-reload|status}" >&2
 	exit 1
 	;;
 esac
