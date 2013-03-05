c22a57de50cad6155e50ac437181e8d7208247bb
Marv Cool <marv@hostin.is>
2013-03-05 16:20:03 +0100

    adss useful fixtures and a testcase

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -18,7 +18,7 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should create user" do
     assert_difference('User.count') do
-      post :create, user: { email: @user.email, name: @user.name, pw: @user.pw }
+      post :create, user: { email: @user.email, name: @user.name }
     end
 
     assert_redirected_to user_path(assigns(:user))
@@ -29,18 +29,19 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should not get edit" do
+    # not logged in, now edit page
     get :edit, id: @user
-    assert_response :success
+    assert_response 302 #redirect
   end
 
   test "should update user" do
-    put :update, id: @user, user: { email: @user.email, name: @user.name, pw: @user.pw }
+    put :update, id: @user, user: { email: @user.email, name: @user.name }
     assert_redirected_to user_path(assigns(:user))
   end
 
   test "should destroy user" do
-    assert_difference('User.count', -1) do
+    assert_difference('AdminUser.count', -1) do
       delete :destroy, id: @user
     end
 
